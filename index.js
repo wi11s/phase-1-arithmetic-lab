5e7b05f75847fd357b1ae983d06acb80969517eb
William <wms274@nyu.edu>
2022-08-28 15:47:00 -0400

    commit tree c579e8f84dcc0f2a2ff787e9252c10632b65e574

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// Write your code here\\
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2;
 
-// Write your code here
+const random = Math.floor(Math.random()*100)+1
+
+let num3 = 14
+let num4 = 5
+const mod = num3%num4
+
+const max = Math.max(20)
